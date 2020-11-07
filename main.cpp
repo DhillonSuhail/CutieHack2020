88133a408bcdaeb48ed68da1fd91630da4ee6a83
Gurman Dhaliwal <gdhal005@ucr.edu>
2020-11-07 12:12:53 -0800

    bruh

--- main.cpp
+++ main.cpp
@@ -8,11 +8,7 @@ int wis = 10;
 int hp = 50;
 
 int main() {
-<<<<<<< HEAD
-    cout << "Welcome to the Game!";
-=======
     cout << "Welcome to Door Dash: Gulag Trials!";
->>>>>>> 183414ea57d20cf2a0d36636ff2f4232a4b2c14d
 
     while (hp > 0)
     {
