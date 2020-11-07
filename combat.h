5c0ba2f9df2709f65ddf91b0778be0c108b9c82c
Suhail-Anand Dhillon <dhillonsuhail@gmail.com>
2020-11-07 14:10:32 -0800

    developing combat

--- combat.h
+++ combat.h
@@ -1,7 +1,96 @@
 class Combat
 {
     int enemyhp = 10 + (rand() % 5) - 2;
-    int enemystr;
-    int enemydex;
-    int enemywis;
+    int enemyStr = 10 + (rand() % 5) - 2;
+    int enemyDex = 10 + (rand() % 5) - 2;
+    int enemyWis = 10 + (rand() % 5) - 2;
+
+    string input = "";
+
+    int damage;
+
+    void initializeCombat()
+    {
+        if (dex >= enemyDex)
+        {
+            playerTurn();
+        }
+
+        else
+        {
+            enemyTurn();
+        }
+        
+    }
+
+    void playerTurn()
+    {
+        cout << "How do you want to attack?" << endl;
+        cout << "1. Punch" << endl << "2. Kick" << "3. Grab" << "4. Taunt";
+        cin >> input;
+
+        if (input == "1")
+        {
+            damage = 2 + ((str-10)/2);
+            
+            if (damage < 1)
+            {
+                damage = 1;
+            }
+
+            enemyhp -= damage;
+            cout << "Your punch deals " << damage << " leaving the enemy at " << enemyhp << " hp.";
+            enemyTurn();
+        }
+
+        else if (input == "2")
+        {
+            damage = 2 + ((str-10)/2);
+            
+            if (damage < 1)
+            {
+                damage = 1;
+            }
+
+            enemyhp -= damage;
+            cout << "Your punch deals " << damage << " leaving the enemy at " << enemyhp << " hp.";
+            enemyTurn();
+        }
+
+        else if (input == "3")
+        {
+            
+        }
+
+        else if (input == "4")
+        {
+            damage = 2 + ((str-10)/2);
+            
+            if (damage < 1)
+            {
+                damage = 1;
+            }
+
+            enemyhp -= damage;
+            cout << "Your punch deals " << damage << " leaving the enemy at " << enemyhp << " hp.";
+            enemyTurn();
+        }
+
+        else
+        {
+            cout << "Not an attack, input 1, 2, 3, or 4 in order to select attack" << endl;
+            playerTurn();
+        }
+    }
+
+    void enemyTurn()
+    {
+
+    }
+
+    void endCombat()
+    {
+
+    }
+    
 };
