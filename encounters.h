264c53cb9a9cd22b773936ed911d12d2775fd082
Gurman Dhaliwal <gdhal005@ucr.edu>
2020-11-07 12:50:30 -0800

    encounters class

--- encounters.h
+++ encounters.h
@@ -4,3 +4,15 @@ class encounters{
 
 };
 
+
+
+
+
+/*
+class MyClass {        // The class
+  public:              // Access specifier
+    void myMethod() {  // Method/function defined inside the class
+      cout << "Hello World!";
+    }
+};
+*/
