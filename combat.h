1a5fa0d7b95baa5a2c21173493bfb8c5ae1ac8af
lorenzog1 <73199903+lorenzog1@users.noreply.github.com>
2020-11-07 13:44:46 -0800

    Update combat.h

--- combat.h
+++ combat.h
@@ -1,11 +1,7 @@
 class Combat
 {
-    int enemyhp;
+    int enemyhp = 10 + (rand() % 5) - 2;
     int enemystr;
     int enemydex;
     int enemywis;
-
-    srand(time(0));
-
-    enemyhp = 10 + (rand() % 5) - 2);
 };
