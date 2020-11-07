5831cc34bfa752341c9e47489c09cd6e165da934
Suhail-Anand Dhillon <dhillonsuhail@gmail.com>
2020-11-07 13:01:12 -0800

    organization

--- combat.h
+++ combat.h
@@ -1,4 +1,12 @@
-class Enemy
+#include <iostream>
+#include <cmath>
+#include <ctime>
+using namespace std;
+
+class Combat
 {
-    
+    int enemyhp;
+    int enemystr;
+    int enemydex;
+    int enemywis;
 };
