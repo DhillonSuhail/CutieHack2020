f0c87c389ceb5cdda3ed134c404247c9bf026c6c
lorenzog1 <73199903+lorenzog1@users.noreply.github.com>
2020-11-07 17:21:36 -0800

    lost 2

--- encounters.h
+++ encounters.h
@@ -114,7 +114,7 @@ int alleyEncounter() {
 int lost() {
   cout << "Uh oh... " << endl;
   cout << "After all that, you have lost your sense of direction..." << endl;
-  cout << "You see a stranger and want to ask for directions..." ,,endl; 
+  cout << "You see a stranger and want to ask for directions..." << endl; 
   cout << "but there is a possibility that they counld have coronavirus :O" << endl;
   cout << endl;
   cout << "Do you.." <<endl;
